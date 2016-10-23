697b0c8afa408e8133417df65747361b2b18029f
thngo <tra.huongngo@gmail.com>
2016-10-23 18:08:12 -0500

    Update Load_Data.R
    Adding R update instruction to make sure packages are not broken
    Adding goodgrant_dictionary_criteria2 dataframe for relevant output

--- Load_Data.R
+++ Load_Data.R
@@ -1,3 +1,15 @@
+####UPDATING R TO MAKE SURE NO PACKAGES IS BROKEN###
+###Run in R-GUI, not RStudio
+### installing/loading the package:
+#if(!require(installr)) {
+#  install.packages("installr"); require(installr)} #load / install+load installr
+#updateR() 
+#version #to check version after updating, making sure things are alright
+### this will start the updating process of your R installation.  
+### It will check for newer versions, and if one is available, will guide you through the decisions you'd need to make.
+
+
+
 ####INSTALLING READ EXCEL PACKAGES####
 ####R core does not come with the ability to read excel
 ####Install packages to your flavor
@@ -17,3 +29,6 @@ str(goodgrant_score) #Learn its structure
 
 goodgrant_dictionary <- read_excel("C:/Users/Tra/Desktop/GoodGrantData/Problem_C__CollegeScorecardDataDictionary_09082015.xlsx")
 str(goodgrant_dictionary) #Learn its structure
+
+####CREATING DESIRED OUTPUT FILES####
+goodgrant_dictionary_criteria2 <- goodgrant_dictionary[c(8,11,15,20,25,118,152,166,267,270,272,274,279,281,283,285,290,292,293,294,295,296:329),] #substracting the rows (of picked criteria)
